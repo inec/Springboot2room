f7e2e74a73fe1629139a3a46d5c450099810caf0
Y-Chi Lu <luyege+github@gmail.com>
2019-10-18 23:20:08 -0500

    xml formatter

--- src/main/java/com/exaeuho/demo/RoomController.java
+++ src/main/java/com/exaeuho/demo/RoomController.java
@@ -3,6 +3,7 @@ package com.exaeuho.demo;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,7 +12,21 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/rooms")
 public class RoomController {
-    private static List<Room> rooms = new ArrayList<>();
+
+     private RoomServices roomServices;
+
+    @Autowired
+    public RoomController(RoomServices roomServices){
+        super();
+        this.roomServices = roomServices;
+    }
+
+    @GetMapping
+    public String getAllRooms(Model model){
+        model.addAttribute("rooms", this.roomServices.getAllRooms());
+        return "rooms";
+    }
+    /*private static List<Room> rooms = new ArrayList<>();
 
     static{
         for(int i=0;i<10;i++){
@@ -23,5 +38,5 @@ public class RoomController {
     public String getAllRooms(Model model){
         model.addAttribute("rooms", rooms);
         return "rooms";
-    }
+    }*/
 }
